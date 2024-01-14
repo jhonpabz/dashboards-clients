07cadbff543ad418312212a492e1060e39bd780e
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2024-01-14 15:08:42 +0800

    Modify home page. Add client variable and use the dashboard component.

--- src/views/home/HomePage.tsx
+++ src/views/home/HomePage.tsx
@@ -1,17 +1,12 @@
-"use client";
-
 import PageContainer from "@/components/PageContainer";
-import { ColumnContainer } from "@/components/dashboard";
-import { configuration } from "@/constants/configuration";
+import { Dashboard } from "@/components/dashboard";
 
 export default function HomePage() {
-  const dashboardTypes = configuration.columns;
+  const client = "pokemon";
 
   return (
     <PageContainer>
-      {dashboardTypes.map((data, index) => (
-        <ColumnContainer key={index} data={data} />
-      ))}
+      <Dashboard client={client} />
     </PageContainer>
   );
 }
