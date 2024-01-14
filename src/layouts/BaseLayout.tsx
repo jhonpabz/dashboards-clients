8b2226eb0c6392499f8fcc427c783bacb09ac09b
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2024-01-14 17:11:38 +0800

    Add app base layout.

---
+++ src/layouts/BaseLayout.tsx
@@ -0,0 +1,12 @@
+import SidebarNav from "@/layouts/SidebarNav";
+
+export default function BaseLayout(props: ComponentsPropsNamespace.BaseLayout) {
+  return (
+    <>
+      <div className="flex min-h-screen flex-1 flex-row">
+        <SidebarNav />
+        {props.children}
+      </div>
+    </>
+  );
+}
