058d45eb214b820bbc81c1035d522c667beea901
JHON CESAR PABLO <jhncsrpbl@gmail.com>
2024-01-14 17:10:14 +0800

    Update tailwind config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,13 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundColor: {
-        main: "#eff2ed",
+        main: "#E5E1DA",
+        secondary: "#AAD7D9",
       },
     },
   },
